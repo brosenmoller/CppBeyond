de88836cf2422eb8d75245da0579b1a7ee683bf0
brosenmoller <rosenmollerben@gmail.com>
2023-11-20 20:54:09 +0100

    Visualize queue length

--- Template_Queue/main.cpp
+++ Template_Queue/main.cpp
@@ -39,17 +39,20 @@ void QueueTest()
     std::cout << "Enqueued 6" << std::endl;
 
     std::cout << queue.Peek() << " is at the front of the Queue" << std::endl;
+    std::cout << queue.Size() << " is the length of the array" << std::endl;
 
     queue.Enqueue(10);
     std::cout << "Enqueued 10" << std::endl;
 
     std::cout << queue.Peek() << " is still at the front of the Queue" << std::endl;
+    std::cout << queue.Size() << " is the length of the array" << std::endl;
 
     int x = queue.Dequeue();
     std::cout << "Dequeued first queue item into x" << std::endl;
 
     std::cout << queue.Peek() << " is now at the front of the Queue" << std::endl;
     std::cout << "Variable X contains: " << x << std::endl;
+    std::cout << queue.Size() << " is the length of the array" << std::endl;
 }
 
 void ArraySortTest()

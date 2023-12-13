7a959f4d1c490080f0b5139f27959c7ffdef416b
brosenmoller <rosenmollerben@gmail.com>
2023-12-13 15:32:34 +0100

    ThreadedInventory - Added comments

--- ThreadedInventory/main.cpp
+++ ThreadedInventory/main.cpp
@@ -7,11 +7,12 @@ std::vector<int> items1{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 std::vector<int> items2{ 11, 12, 13, 14, 15, 16, 17, 18, 19 };
 std::vector<int> items3{ 1, 2, 3, 4, 5 };
 
-void ItemAdder(std::vector<int> items)
+void ItemAdder(std::vector<int> items, long delay)
 {
 	for (int i = 0; i < items.size(); i++)
 	{
 		inventory.Add(items[i]);
+		std::this_thread::sleep_for(std::chrono::milliseconds(delay));  // Simulate difficult calculation
 	}
 }
 
@@ -20,18 +21,23 @@ void ItemRemover(std::vector<int> items)
 	for (int i = 0; i < items.size(); i++)
 	{
 		inventory.Remove(items[i]);
+		std::this_thread::sleep_for(std::chrono::milliseconds(1)); // Simulate difficult calculation
 	}
 }
 
 
 int main()
 {
-	std::thread adder1Thread(ItemAdder, items1);
-	std::thread adder2Thread(ItemAdder, items2);
+	std::thread adder1Thread(ItemAdder, items1, 2);
+	std::thread adder2Thread(ItemAdder, items2, 1);
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
 	std::thread remover1Thread(ItemRemover, items3);
 
 	std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
+	// Check Current and first item of list to see if this is possible during the other threads runtime
 	std::cout << "Current First: " << inventory.Get(0) << std::endl;
 	std::cout << "Current Last: " << inventory.GetLast() << std::endl;
 
